3545b63ad50cf6e94fce0cd92267a9d548f1e069
Christian Koop <contact@sprax2013.de>
2023-09-22 16:39:57 +0200

    Update applicationId, namespace and class packages
    Using the same name as the current app for this "recode"

--- Android/app/src/main/java/de/sprax2013/hob_wa_sticker/StickerPackLoader.java
+++ Android/app/src/main/java/de/sprax2013/hob_wa_sticker/StickerPackLoader.java
@@ -6,7 +6,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-package com.example.samplestickerapp;
+package de.sprax2013.hob_wa_sticker;
 
 import android.content.ContentResolver;
 import android.content.Context;
@@ -24,21 +24,21 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 
-import static com.example.samplestickerapp.StickerContentProvider.ANDROID_APP_DOWNLOAD_LINK_IN_QUERY;
-import static com.example.samplestickerapp.StickerContentProvider.ANIMATED_STICKER_PACK;
-import static com.example.samplestickerapp.StickerContentProvider.AVOID_CACHE;
-import static com.example.samplestickerapp.StickerContentProvider.IMAGE_DATA_VERSION;
-import static com.example.samplestickerapp.StickerContentProvider.IOS_APP_DOWNLOAD_LINK_IN_QUERY;
-import static com.example.samplestickerapp.StickerContentProvider.LICENSE_AGREENMENT_WEBSITE;
-import static com.example.samplestickerapp.StickerContentProvider.PRIVACY_POLICY_WEBSITE;
-import static com.example.samplestickerapp.StickerContentProvider.PUBLISHER_EMAIL;
-import static com.example.samplestickerapp.StickerContentProvider.PUBLISHER_WEBSITE;
-import static com.example.samplestickerapp.StickerContentProvider.STICKER_FILE_EMOJI_IN_QUERY;
-import static com.example.samplestickerapp.StickerContentProvider.STICKER_FILE_NAME_IN_QUERY;
-import static com.example.samplestickerapp.StickerContentProvider.STICKER_PACK_ICON_IN_QUERY;
-import static com.example.samplestickerapp.StickerContentProvider.STICKER_PACK_IDENTIFIER_IN_QUERY;
-import static com.example.samplestickerapp.StickerContentProvider.STICKER_PACK_NAME_IN_QUERY;
-import static com.example.samplestickerapp.StickerContentProvider.STICKER_PACK_PUBLISHER_IN_QUERY;
+import static de.sprax2013.hob_wa_sticker.StickerContentProvider.ANDROID_APP_DOWNLOAD_LINK_IN_QUERY;
+import static de.sprax2013.hob_wa_sticker.StickerContentProvider.ANIMATED_STICKER_PACK;
+import static de.sprax2013.hob_wa_sticker.StickerContentProvider.AVOID_CACHE;
+import static de.sprax2013.hob_wa_sticker.StickerContentProvider.IMAGE_DATA_VERSION;
+import static de.sprax2013.hob_wa_sticker.StickerContentProvider.IOS_APP_DOWNLOAD_LINK_IN_QUERY;
+import static de.sprax2013.hob_wa_sticker.StickerContentProvider.LICENSE_AGREENMENT_WEBSITE;
+import static de.sprax2013.hob_wa_sticker.StickerContentProvider.PRIVACY_POLICY_WEBSITE;
+import static de.sprax2013.hob_wa_sticker.StickerContentProvider.PUBLISHER_EMAIL;
+import static de.sprax2013.hob_wa_sticker.StickerContentProvider.PUBLISHER_WEBSITE;
+import static de.sprax2013.hob_wa_sticker.StickerContentProvider.STICKER_FILE_EMOJI_IN_QUERY;
+import static de.sprax2013.hob_wa_sticker.StickerContentProvider.STICKER_FILE_NAME_IN_QUERY;
+import static de.sprax2013.hob_wa_sticker.StickerContentProvider.STICKER_PACK_ICON_IN_QUERY;
+import static de.sprax2013.hob_wa_sticker.StickerContentProvider.STICKER_PACK_IDENTIFIER_IN_QUERY;
+import static de.sprax2013.hob_wa_sticker.StickerContentProvider.STICKER_PACK_NAME_IN_QUERY;
+import static de.sprax2013.hob_wa_sticker.StickerContentProvider.STICKER_PACK_PUBLISHER_IN_QUERY;
 
 class StickerPackLoader {
 
